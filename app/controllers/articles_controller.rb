fe7201b42f9fde8b6e12f0ac25a4f2db37c81f53
Lucius Choi <rorlab@gmail.com>
2014-12-10 10:05:42 +0900

    76회 Biweekly Lecture

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -18,6 +22,12 @@ class ArticlesController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article), notice: "성공적으로 업데이트 되었습니다."
   end
 
   private
